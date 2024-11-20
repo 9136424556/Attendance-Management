8a48187572414e6007b0c584dcbb247fd20292e4
harkeye <h.suzuki.20030701@gmail.com>
2024-11-20 23:57:02 +0900

    一般ユーザーのSeederを追加

--- src/database/seeders/UsersTableSeeder.php
+++ src/database/seeders/UsersTableSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Seeder;
 
 class UsersTableSeeder extends Seeder
@@ -13,6 +15,11 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $param = [
+            'name' => 'スタッフ１',
+            'email' => 'staffuser@email.com',
+            'password' => Hash::make('staffuser1'),
+        ];
+        DB::table('users')->insert($param);
     }
 }
